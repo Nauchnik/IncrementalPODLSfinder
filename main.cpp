d37e7864b640d02ad4d139da743e71517a805333
Oleg Zaikin <zaikin.icc@gmail.com>
2016-01-03 02:27:50 +0800

    + Incremental finding all PODLS for a DLS

--- main.cpp
+++ main.cpp
@@ -10,11 +10,15 @@ As a result for all such DLSs own CNF is constructed. The goal is to find for ev
 #include <sstream>
 #include <fstream>
 #include <vector>
-#include <set>
+#include <ctime>
 #include "odls_sequential.h"
 
 std::vector<int> makeLiterals(dls cur_dls);
 void normalizeLS(dls &cur_DLS);
+std::vector<dls> getSetUniqueDLS(std::vector<odls_pair> odls_pair_vec);
+void processDLS(unsigned cnf_var_count, std::stringstream &init_cnf_sstream, dls cur_dls);
+dls getDLSfromSolutionFile(std::string solutionfile_name);
+void printDLS(dls cur_dls);
 
 int main( int argc, char **argv )
 {
@@ -63,71 +67,83 @@ int main( int argc, char **argv )
 	odls_seq.readOdlsPairs(known_podls_file_name, odls_pair_vec);
 	// construct a set of unique DLSs from these pairs
 	std::vector<dls> unique_dls_vec;
-	std::vector<std::vector<int>> forbidden_DLS_literals_vec;
-	bool isInsertRequired;
 
-	for (auto &x : odls_pair_vec) {
-		if (x.dls_1[0] != "0123456789")	{
-			normalizeLS(x.dls_1);
-			for (auto &y : x.dls_1) {
-				for (auto &y2 : y)
-					std::cout << y2 << " ";
-				std::cout << std::endl;
-			}
-			std::cout << std::endl;
-		}
-		if (x.dls_2[0] != "0123456789") {
-			normalizeLS(x.dls_2);
-			for (auto &y : x.dls_2) {
-				for (auto &y2 : y)
-					std::cout << y2 << " ";
-				std::cout << std::endl;
-			}
-			std::cout << std::endl;
-		}
-		isInsertRequired = true;
-		for (auto &y : unique_dls_vec)
-			if (y == x.dls_1) {
-				isInsertRequired = false;
-				break;
-			}
-		if (isInsertRequired) {
-			unique_dls_vec.push_back(x.dls_1);
-			forbidden_DLS_literals_vec.push_back(makeLiterals(x.dls_2));
-		}	
-		//
-		isInsertRequired = true;
-		for (auto &y : unique_dls_vec)
-			if (y == x.dls_2) {
-				isInsertRequired = false;
-				break;
-			}
-		if (isInsertRequired) {
-			unique_dls_vec.push_back(x.dls_2);
-			forbidden_DLS_literals_vec.push_back(makeLiterals(x.dls_1));
-		}
-	}
+	// get unique DLSs from the known pairs of ODLS
+	unique_dls_vec = getSetUniqueDLS( odls_pair_vec );
+	
+	for (auto &x : unique_dls_vec)
+		processDLS(cnf_var_count, init_cnf_sstream, x);
+	
+	return 0;
+}
+
+void processDLS(unsigned cnf_var_count, std::stringstream &init_cnf_sstream, dls cur_dls)
+{
 	// make CNF for every unique DLS
 	unsigned row_index, column_index;
 	std::ofstream cnf_with_known_dls;
-	std::string cnf_with_known_dls_name;
-	std::stringstream cur_dls_clauses_sstream, forbidden_assumption_clauses_sstream, dls_comments_sstream;
+	std::ifstream out_incremental_file;
+	std::vector<int> forbidden_DLS_literals;
 	
-	unsigned new_cnf_variable = cnf_var_count + 1; // an additional variable for encoding conjunction into set of clauses
-	unsigned k = 0;
-	for (auto &x : unique_dls_vec) {
-		dls_comments_sstream << "c known DLS:" << std::endl;
-		dls_comments_sstream << "c 0 1 2 3 4 5 6 7 8 9" << std::endl;
-		for (row_index = 1; row_index < x.size(); row_index++) { // skip row # 0 - it if fixed already
-			dls_comments_sstream << "c ";
-			for (column_index = 0; column_index < x[0].size(); column_index++) {
-				cur_dls_clauses_sstream << 100 * row_index + 10 * column_index + (x[row_index][column_index] - 48) + 1 << " 0\n"; // 1000*0 + ...
-				dls_comments_sstream << x[row_index][column_index];
-				if (column_index != x.size()-1)
-					dls_comments_sstream << " ";
-			}
-			dls_comments_sstream << std::endl;
+	std::stringstream cur_dls_clauses_sstream, forbidden_assumption_clauses_sstream, dls_comments_sstream;
+	unsigned new_cnf_variable;
+	bool isSATfound = false;
+	new_cnf_variable = cnf_var_count;
+	std::string cnf_with_known_dls_name = "PODLS_known_DLS_tmp.cnf";
+	std::string out_incremental_file_name = "PODLS_tmp.out";
+	std::string system_str;
+	dls dls_from_output;
+	unsigned sat_count = 0;
+
+	std::cout << "---" << std::endl;
+	
+	dls_comments_sstream << "c 0 1 2 3 4 5 6 7 8 9" << std::endl;
+	for (row_index = 1; row_index < cur_dls.size(); row_index++) { // skip row # 0 - it if fixed already
+		dls_comments_sstream << "c ";
+		for (column_index = 0; column_index < cur_dls[0].size(); column_index++) {
+			cur_dls_clauses_sstream << 100 * row_index + 10 * column_index + (cur_dls[row_index][column_index] - 48) + 1 << " 0\n"; // 1000*0 + ...
+			dls_comments_sstream << cur_dls[row_index][column_index];
+			if (column_index != cur_dls.size() - 1)
+				dls_comments_sstream << " ";
 		}
+		dls_comments_sstream << std::endl;
+	}
+	
+	for (;;)
+	{ // incremental iterations for finding all SAT assignments of the CNF
+		new_cnf_variable++; // an additional variable for encoding conjunction into set of clauses
+		
+		// write obtained clauses to a CNF
+		cnf_with_known_dls.open(cnf_with_known_dls_name.c_str(), std::ios_base::out);
+		cnf_with_known_dls << dls_comments_sstream.str();
+		cnf_with_known_dls << init_cnf_sstream.str();
+		cnf_with_known_dls << cur_dls_clauses_sstream.str();
+		// first time forbidden_assumption_clauses_sstream is empty
+		cnf_with_known_dls << forbidden_assumption_clauses_sstream.str();
+		cnf_with_known_dls.clear();
+		cnf_with_known_dls.close();
+		
+		// fix CNF
+		system_str = "./fix_cnf " + cnf_with_known_dls_name;
+		std::cout << "system_str " << system_str << std::endl;
+		system(system_str.c_str());
+		// launch SAT solver
+		system_str = "./plingeling " + cnf_with_known_dls_name + " &> " + out_incremental_file_name;
+		std::cout << "system_str " << system_str << std::endl;
+		system(system_str.c_str());
+		// read output of a SAT solver
+		dls_from_output = getDLSfromSolutionFile(out_incremental_file_name);
+		printDLS(dls_from_output);
+		isSATfound = (dls_from_output.size() > 0) ? true : false;
+		if (!isSATfound) {
+			std::cout << "UNSAT" << std::endl;
+			break;
+		}
+		sat_count++;
+		std::cout << "current sat_count " << sat_count << std::endl;
+		forbidden_DLS_literals = makeLiterals(dls_from_output);
+		std::cout << "forbidden_DLS_literals.size() " << forbidden_DLS_literals.size() << std::endl;
+
 		// construct clauses for forbidding current assumption
 		// write clause A == B -> (A or not(B)) and ( not(A) or B ) where A is a new variable
 		// if here B, for example, is ( x1 or (notx2) ) then A == (x1 or not(x2) ) =>
@@ -135,38 +151,20 @@ int main( int argc, char **argv )
 		// the last two clauses were made with the help of distributive rule
 		// equality condition clause with positive phase of the new variable
 		forbidden_assumption_clauses_sstream << new_cnf_variable << " ";
-		for (unsigned j = 0; j < forbidden_DLS_literals_vec[k].size(); j++)
-			forbidden_assumption_clauses_sstream << "-" << forbidden_DLS_literals_vec[k][j] << " ";
+		for (unsigned j = 0; j < forbidden_DLS_literals.size(); j++)
+			forbidden_assumption_clauses_sstream << "-" << forbidden_DLS_literals[j] << " ";
 		forbidden_assumption_clauses_sstream << "0" << std::endl;
 		// equality condition clauses with negative phase of the new variable
-		for (unsigned j = 0; j < forbidden_DLS_literals_vec[k].size(); j++) {
+		for (unsigned j = 0; j < forbidden_DLS_literals.size(); j++) {
 			forbidden_assumption_clauses_sstream << "-" << new_cnf_variable << " "; // negative phase cause De Morgana rule
-			forbidden_assumption_clauses_sstream << forbidden_DLS_literals_vec[k][j] << " 0" << std::endl;
+			forbidden_assumption_clauses_sstream << forbidden_DLS_literals[j] << " 0" << std::endl;
 		}
 		// add negative phase of the new variable - we must forbid it
 		forbidden_assumption_clauses_sstream << "-" << new_cnf_variable << " 0" << std::endl;
-		// write obtained clauses to a CNF
-		sstream << k;
-		cnf_with_known_dls_name = "PODLS_known_DLS_" + sstream.str() + ".cnf";
-		cnf_with_known_dls.open(cnf_with_known_dls_name.c_str(), std::ios_base::out);
-		cnf_with_known_dls << dls_comments_sstream.str();
-		cnf_with_known_dls << init_cnf_sstream.str();
-		cnf_with_known_dls << cur_dls_clauses_sstream.str();
-		cnf_with_known_dls << forbidden_assumption_clauses_sstream.str();
-		cnf_with_known_dls.close(); 
-		cnf_with_known_dls.clear();
-		forbidden_assumption_clauses_sstream.clear();
-		forbidden_assumption_clauses_sstream.str("");
-		sstream.str("");
-		sstream.clear();
-		dls_comments_sstream.str("");
-		dls_comments_sstream.clear();
-		cur_dls_clauses_sstream.clear();
-		cur_dls_clauses_sstream.str("");
-		k++;
+		// don't clear forbidden_assumption_clauses_sstream, we need to increment it every iteration
+		forbidden_DLS_literals.clear();
 	}
-	
-	return 0;
+	std:: cout << "final sat_count " << sat_count << std::endl;
 }
 
 // normalize LS by 1st row
@@ -199,4 +197,106 @@ std::vector<int> makeLiterals(dls cur_dls)
 			result_vec.push_back( 1000 * 1 + 100 * row_index + 10 * column_index + (cur_dls[row_index][column_index] - 48) + 1 );
 	}
 	return result_vec;
+}
+
+std::vector<dls> getSetUniqueDLS(std::vector<odls_pair> odls_pair_vec)
+{
+	std::vector<dls> unique_dls_vec;
+
+	bool isInsertRequired;
+
+	for (auto &x : odls_pair_vec) {
+		if (x.dls_1[0] != "0123456789") {
+			normalizeLS(x.dls_1);
+			for (auto &y : x.dls_1) {
+				for (auto &y2 : y)
+					std::cout << y2 << " ";
+				std::cout << std::endl;
+			}
+			std::cout << std::endl;
+		}
+		if (x.dls_2[0] != "0123456789") {
+			normalizeLS(x.dls_2);
+			for (auto &y : x.dls_2) {
+				for (auto &y2 : y)
+					std::cout << y2 << " ";
+				std::cout << std::endl;
+			}
+			std::cout << std::endl;
+		}
+		isInsertRequired = true;
+		for (auto &y : unique_dls_vec)
+			if (y == x.dls_1) {
+				isInsertRequired = false;
+				break;
+			}
+		if (isInsertRequired)
+			unique_dls_vec.push_back(x.dls_1);
+		//
+		isInsertRequired = true;
+		for (auto &y : unique_dls_vec)
+			if (y == x.dls_2) {
+				isInsertRequired = false;
+				break;
+			}
+		if (isInsertRequired)
+			unique_dls_vec.push_back(x.dls_2);
+	}
+
+	return unique_dls_vec;
+}
+
+dls getDLSfromSolutionFile( std::string solutionfile_name)
+{
+	// check solution
+	std::stringstream sstream;
+	std::ifstream solutionfile(solutionfile_name.c_str(), std::ios_base::in);
+	std::string str;
+	dls new_dls;
+	std::string dls_row;
+	int val;
+
+	if (!solutionfile.is_open()) {
+		std::cerr << "solutionfile " << solutionfile_name << " not open" << std::endl;
+		exit(1);
+	}
+	
+	while (std::getline(solutionfile, str)) {
+		if ((str[0] == 'v') && (str[1] == ' ')) {
+			sstream << str.substr(2);
+			while (sstream >> val) {
+				//if ( ( val >= 2001 ) && ( val <= 3000 ) ) { // for 3rd DLS
+				if ((val >= 1001) && (val <= 2000)) {
+					val = val % 10 ? (val % 10) - 1 : 9;
+					dls_row.push_back('0' + val);
+				}
+				if (dls_row.size() == 10) {
+					new_dls.push_back(dls_row);
+					//std::cout << dls_row << std::endl;
+					dls_row = "";
+				}
+			}
+			sstream.clear(); sstream.str("");
+		}
+	}
+	/*std::cout << std::endl;
+	for (auto &x : new_dls) {
+		for (unsigned i = 0; i<x.size(); i++)
+			std::cout << x[i] << " ";
+		std::cout << std::endl;
+	}*/
+	
+	solutionfile.close();
+	return new_dls;
+}
+
+void printDLS(dls cur_dls)
+{
+	for (unsigned i = 0; i < cur_dls.size(); i++) {
+		for (unsigned j = 0; j < cur_dls[i].size(); j++) {
+			std::cout << cur_dls[i][j];
+			if (j != cur_dls[i].size() - 1)
+				std::cout << " ";
+		}
+	}
 }
